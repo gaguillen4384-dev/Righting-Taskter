2c7524a6b91293094776bf79596c3b637da2113d
gaguillen4384-dev <gaguillen4384-dev@gmail.com>
2023-07-15 21:11:43 -0400

    monthly merge

---
+++ Taskter/Tests/Manager.Tests/ProjectManager/NaturalValues/NaturalValues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Manager.Tests.ProjectManager
+{
+    internal static class NaturalValues
+    {
+        public static string AppsettingsLocation = @"C:\WorkSpace\Righting-Taskter\Taskter\Manager.Tests\ProjectManager\appsettings.json";
+
+        public static int NumberOfProjectsToUse = 5;
+
+        public static int Single = 1;
+
+        public static string PrjAcronymToUse = "projectAcronym";
+
+        public static string PrjName = "Project";
+
+        public static string NewPrjAcronymToUse = "NewProjectAcronym";
+
+        public static string NewPrjName = "NewProject";
+
+        public static Guid PrjGuid = new Guid("05ec1a69-e74a-4c74-9067-0980056bc033");
+    }
+}
